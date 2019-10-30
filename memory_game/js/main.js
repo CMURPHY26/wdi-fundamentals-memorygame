b19d718a0beb3a061397985a84bdb97cd20cd632
Chris <cl@Chriss-MacBook-Pro.local>
2019-10-29 21:20:39 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+
+console.log("User flipped: " + cardOne);
+console.log("User flipped: " + cardFour);
